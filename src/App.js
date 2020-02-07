9e9d00245849fee5827bd36ca73825d9f4e90aad
Lazaro Carvalho <lazaro.oliveiras@hotmail.com>
2020-02-07 14:37:21 -0300

    construindo uma tabela e preenchendo-a com dados buscados na API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+   constructor() {
+      super();
+
+      this.state = {
+         lista: []
+      }
+   }
+
+   async componentDidMount() {
+      let resposta = await fetch('http://localhost:3000/series');
+      const series = await resposta.json();
+      this.setState({lista : series})
+   }
+
+   render() {
+      console.log("Estou sendo renderizado");
+      return (
+         <div className="App">
+            <form method="POST">
+               <div className='form'>
+                  <label htmlFor="nome">Nome</label>
+                  <input type="text" id="nome" name="nome"></input>
+                  <label htmlFor="sinopse">Sinopse</label>
+                  <input type="text" id="sinopse" name="sinopse"></input>
+                  <label htmlFor="ano_lancamento">Ano de lançamento</label>
+                  <input type="text" id="ano_lancamento" name="ano_lancamento"></input>
+                  <label htmlFor="temporadas">Temporadas</label>
+                  <textarea id="temporadas" name="temporadas"></textarea>
+                  <button type="submit">Salvar</button>
+               </div>
+            </form>
+            <div className="list">
+               <table>
+                  <thead>
+                     <tr>
+                        <th>Nome</th>
+                        <th>Sinopse</th>
+                        <th>lançamento</th>
+                        <th>Temporadas</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {this.state.lista.map(serie => {
+                        return (
+                           <tr>
+                              <td>{serie.id}</td>
+                              <td>{serie.nome}</td>
+                              <td>{serie.sinopse}</td>
+                              <td>{serie.lancamento}</td>
+                              <td>{serie.temporadas}</td>
+                           </tr>
+                        )
+                     })}
+                  </tbody>
+               </table>
+            </div>
+         </div>
+      );
+   }
 }
 
 export default App;
